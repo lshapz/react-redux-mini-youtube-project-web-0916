238db3b872441a353e6f19751da99110f87d1149
lshapz <laurashapiro01@gmail.com>
2016-11-27 14:10:06 -0500

    four not five on sidebar

--- src/display.js
+++ src/display.js
@@ -3,13 +3,13 @@ export default function ShowVideo(props){
 
 // debugger
    var big = props.videos.filter(vid=>{return vid.primary === true})
-   var big_id = big[0] ? big[0].id :  "wRP6egIEABk" 
+   var big_id = big[0] ? big[0].id :  "dHg50mdODFM" 
  
  var show = props.videos.filter(vid=>{return vid.primary === false}).map((vid, index) =>{
   return <div key={index} onClick={props.newBig} id={vid.id}>
-<p id={vid.id}>{vid.title}</p>
-<p id={vid.id}><img className={"video-item " + vid.id } id={vid.id} src={vid.thumb} alt={vid.title} /></p>
-         </div>
+    <p id={vid.id}>{vid.title}</p>
+    <p id={vid.id}><img className={"video-item " + vid.id } id={vid.id} src={vid.thumb} alt={vid.title} /></p>
+  </div>
  })
 
 
--- src/root-reducer.js
+++ src/root-reducer.js
@@ -9,6 +9,7 @@ function videos(state = { videos: []}, action)
       case 'RECEIVE_VIDEOS':
         var videos = action.payload.map((vid)=>{
         return {id: vid.id.videoId, thumb: vid.snippet.thumbnails.default.url, title: vid.snippet.title, primary: false} })
+        videos[0].primary = true
         return Object.assign({}, state, {videos: videos})
       case 'NEW_PRIMARY':
         var new_state = state.videos.filter(thing=>{return thing.id !== action.payload }).map(thing=>{ return {id: thing.id, thumb: thing.thumb, title: thing.title, primary: false}  })
